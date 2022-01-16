024c08ace40a095a4a7602b9bc1983649bfcf78b
Rhaniel Farias <rhanielfarias@gmail.com>
2022-01-16 14:52:05 -0300

    Identando o javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.querySelector ('button');
-traduzirvivo = (data) => {
+        traduzirvivo = (data) => {
     if(data.status == 'unknown'){
         return 'Não sabemos';
     }else if(data.status == 'Alive'){
@@ -10,32 +10,32 @@ traduzirvivo = (data) => {
 }
 
 gerarValorAleatorio = () => {
-return Math.floor (Math.random() * 671);
+    return Math.floor (Math.random() * 671);
 }
 pegarPersonagem =(numero) =>{
-let numeroAleatorio = gerarValorAleatorio ();
-return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-method: 'GET',
-headers: {
-Accept: 'application/json',
+    let numeroAleatorio = gerarValorAleatorio ();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    method: 'GET',
+    headers: {
+    Accept: 'application/json',
 "Content-type": 'application/json'
 }
 }).then((response) => response.json()).then((data) =>{
-const imagem = document.querySelector (`#imagem${numero}`);
-const nomeDoPersonagem = document.querySelector (`#nome${numero}`);
-const especie = document.querySelector (`#especie${numero}`);
-const vivo = document.querySelector (`#vivo${numero}`);
+    const imagem = document.querySelector (`#imagem${numero}`);
+    const nomeDoPersonagem = document.querySelector (`#nome${numero}`);
+    const especie = document.querySelector (`#especie${numero}`);
+    const vivo = document.querySelector (`#vivo${numero}`);
 
-imagem.src=data.image;
-imagem.alt=data.name;
-nomeDoPersonagem.innerHTML = data.name;
-especie.innerHTML = data.species;
-vivo.innerHTML =traduzirvivo(data);
+    imagem.src=data.image;
+    imagem.alt=data.name;
+    nomeDoPersonagem.innerHTML = data.name;
+    especie.innerHTML = data.species;
+    vivo.innerHTML =traduzirvivo(data);
 });
 }
 pegarTresPersonagens =() =>{
-pegarPersonagem(1);
-pegarPersonagem(2);
-pegarPersonagem(3);
+    pegarPersonagem(1);
+    pegarPersonagem(2);
+    pegarPersonagem(3);
 }
 botao.onclick =pegarTresPersonagens;
